e0b1b5a7c6713ee1f8d3c0a22389ae80527661e1
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-07 00:35:03 +0100

    package: Move wallpapers into theme module

--- modules/home/theme/north-01/default.nix
+++ modules/home/theme/north-01/default.nix
@@ -5,24 +5,46 @@
   ...
 }:
 let
-  inherit (lib) types;
+  mkWallpaperDrv =
+    wallpaper:
+    pkgs.stdenvNoCC.mkDerivation {
+      pname = "${wallpaper.name}";
+      version = "0.0.0";
+
+      src = pkgs.fetchurl {
+        inherit (wallpaper) sha256;
+        name = "${wallpaper.name}-source";
+        url = "${wallpaper.url}";
+      };
+
+      unpackPhase = "true";
+
+      buildPhase = ''
+        mkdir -p $out
+        cp $src $out/original
+        ${pkgs.imagemagick}/bin/magick $src -blur 0x8 $out/blurred
+      '';
+    };
+
+  wallpaper = mkWallpaperDrv {
+    name = "francesco-ungaro-lcQzCo-X1vM-unsplash";
+    ext = "jpg";
+    url = "https://images.unsplash.com/photo-1729839472414-4f28edcb5b80?ixlib=rb-4.1.0&q=85&fm=jpg&crop=entropy&cs=srgb&dl=francesco-ungaro-lcQzCo-X1vM-unsplash.jpg&w=2400";
+    sha256 = "a36d6e0231bc57900e9725675664d9fa075996f4fee1bd96580f183eac5b4685";
+  };
 
   cfg = config.nixsys.home.theme.north-01;
 in
 {
   options.nixsys.home.theme.north-01 = {
     enable = lib.mkEnableOption "nixsys.home.theme.north-01";
-    wallpaper = lib.mkOption {
-      type = types.package;
-      default = pkgs.local.wallpapers.francesco-ungaro-lcQzCo-X1vM-unsplash;
-    };
   };
 
   config = lib.mkIf cfg.enable {
     nixsys.home.theme.active = lib.mkForce {
-      desktop.bg = "${cfg.wallpaper}/original";
+      desktop.bg = "${wallpaper}/original";
       lock-screen = {
-        bg = "${cfg.wallpaper}/blurred";
+        bg = "${wallpaper}/blurred";
         font = {
           name = "Titillium";
           package = pkgs.local.titillium-font;
--- packages/default.nix
+++ packages/default.nix
@@ -6,5 +6,4 @@
   awesome-terminal-fonts = pkgs.callPackage ./awesome-terminal-fonts { };
   polybar-module-battery-combined-sh = pkgs.callPackage ./polybar-module-battery-combined-sh { };
   titillium-fonts = pkgs.callPackage ./titillium-fonts { };
-  wallpapers = pkgs.callPackage ./wallpapers { };
 }
