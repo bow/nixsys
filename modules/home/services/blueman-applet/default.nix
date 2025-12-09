fdf48fc518c41f5415fcc19faac16c646ce84dc0
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-09 19:41:08 +0100

    module(home): Set blueman-applet default to depend on both desktop and bluetooth

--- modules/home/services/blueman-applet/default.nix
+++ modules/home/services/blueman-applet/default.nix
@@ -8,22 +8,22 @@ let
   libcfg = lib.nixsys.home;
 
   bluetoothEnabled = libcfg.isBluetoothEnabled config;
+  desktopEnabled = libcfg.isDesktopEnabled config;
 
   cfg = config.nixsys.home.services.blueman-applet;
 in
 {
   options.nixsys.home.services.blueman-applet = {
     enable = lib.mkEnableOption "nixsys.home.services.blueman-applet" // {
-      default = libcfg.isDesktopEnabled config;
+      default = bluetoothEnabled && desktopEnabled;
     };
     package = lib.mkPackageOption pkgs "blueman" { };
   };
 
-  config = lib.mkIf (cfg.enable && bluetoothEnabled) {
+  config = lib.mkIf cfg.enable {
     services.blueman-applet = {
       inherit (cfg) package;
       enable = true;
     };
   };
 }
-
