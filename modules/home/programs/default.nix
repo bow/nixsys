4b92574446cad3eba80db4bb44d10c683bcb9710
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-14 08:14:51 +0100

    module(home): Add auto-generation of program modules for simple programs

---
+++ modules/home/programs/default.nix
@@ -0,0 +1,125 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) types;
+
+  # This is for programs that we just want to put behind an enable / disable flag and
+  # no other options.
+  mkProgramModule =
+    pkgName: pkg:
+    (
+      { config, ... }:
+      let
+        pcfg = config.nixsys.home.programs.${pkgName};
+      in
+      {
+        options.nixsys.home.programs.${pkgName} = {
+          enable = lib.mkEnableOption "nixsys.home.programs.${pkgName}";
+          package = lib.mkOption {
+            type = types.package;
+            default = pkg;
+          };
+        };
+
+        config = lib.mkIf pcfg.enable {
+          home.packages = [ pcfg.package ];
+        };
+      }
+    );
+
+  mkProgramModuleImports = args: builtins.attrValues (builtins.mapAttrs mkProgramModule args);
+in
+{
+  # 'inherit' because that's the only reliable way to map package names we use here to the actual
+  # packages we want ~ without interference from env-wrapping or aliases.
+  imports = mkProgramModuleImports {
+    inherit (pkgs)
+      # Backup tools.
+      restic
+
+      # Base tools.
+      coreutils
+      curl
+      file
+      gawk
+      gnugrep
+      gnused
+      jq
+      vim
+      which
+
+      # Chat.
+      weechat
+
+      # Media editors.
+      pdftk
+      imagemagick
+
+      # Network clients.
+      aria2
+      elinks
+      wget
+
+      # Nix.
+      nh
+
+      # Ops.
+      btop
+      dmidecode
+      dnsmasq
+      dua
+      duf
+      ethtool
+      eza
+      fd
+      findutils
+      hexyl
+      htop
+      iftop
+      inetutils
+      iotop
+      ipcalc
+      iperf3
+      iproute2
+      ldns
+      lshw
+      ltrace
+      mtr
+      nmap
+      ntfs3g
+      openssl
+      pciutils
+      pv
+      socat
+      strace
+      sysstat
+      tree
+      usbutils
+      whois
+      # Compression tools.
+      gzip
+      lzip
+      p7zip
+      unrar
+      unzip
+      xz
+      zip
+      zstd
+
+      # Encryption.
+      age
+      gnupg
+      pass
+      sequoia-sq
+
+      # Virtualization.
+      distrobox
+      nerdctl
+      packer
+      virt-viewer
+      ;
+  };
+}
