e8e1d8f9929e76d4dd3cba62cdb2b6376327dc76
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-17 20:20:31 +0100

    overlay: Disable tests for rustup

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,10 +13,16 @@
 
   modifications = _final: prev: {
     # To speed up builds.
+    rustup = prev.rustup.overrideAttrs (_: {
+      doCheck = false;
+    });
     terraform = prev.terraform.overrideAttrs (_: {
       doCheck = false;
     });
     unstable = prev.unstable // {
+      rustup = prev.unstable.rustup.overrideAttrs (_: {
+        doCheck = false;
+      });
       terraform = prev.unstable.terraform.overrideAttrs (_: {
         doCheck = false;
       });
