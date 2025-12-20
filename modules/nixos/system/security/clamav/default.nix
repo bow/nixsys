62d0ee4fc592434b3e631c9b89a31fff4bf87605
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-20 17:13:34 +0100

    module(nixos): Add clamav module

---
+++ modules/nixos/system/security/clamav/default.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) types;
+
+  cfg = config.nixsys.system.security.clamav;
+in
+{
+  options.nixsys.system.security.clamav = {
+    enable = lib.mkEnableOption "nixsys.system.security.clamav";
+    package = lib.mkPackageOption pkgs "clamav" { };
+
+    enable-scanner = lib.mkOption {
+      type = types.bool;
+      default = false;
+    };
+    enable-updater = lib.mkOption {
+      type = types.bool;
+      default = true;
+    };
+    enable-on-access-scan = lib.mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    services.clamav = {
+      daemon.enable = true;
+      clamonacc.enable = cfg.enable-on-access-scan;
+      scanner.enable = cfg.enable-scanner;
+      updater.enable = cfg.enable-updater;
+    };
+  };
+}
