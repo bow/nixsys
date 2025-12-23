7b5d6e495b2514729b4c9135545f422ce3cb39fe
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-23 21:27:09 +0100

    module(home): Update Thunar settings to use plugins

--- modules/home/programs/thunar/default.nix
+++ modules/home/programs/thunar/default.nix
@@ -10,15 +10,19 @@ in
 {
   options.nixsys.home.programs.thunar = {
     enable = lib.mkEnableOption "nixsys.home.programs.thunar";
-    package = lib.mkPackageOption pkgs "xfce.thunar" { };
+    package = lib.mkPackageOption pkgs.xfce "thunar" { };
   };
 
   config = lib.mkIf cfg.enable {
+
     home.packages = [
-      pkgs.xfce.thunar
-      pkgs.xfce.thunar-archive-plugin
-      pkgs.xfce.thunar-dropbox-plugin
-      pkgs.xfce.thunar-volman
+      (cfg.package.override {
+        thunarPlugins = [
+          pkgs.xfce.thunar-archive-plugin
+          pkgs.xfce.thunar-dropbox-plugin
+          pkgs.xfce.thunar-volman
+        ];
+      })
     ];
   };
 }
