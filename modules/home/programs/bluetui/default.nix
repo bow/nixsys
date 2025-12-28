bfd1596032c4ee704622b674ba95c76a505ebfa3
Wibowo Arindrarto <contact@arindrarto.dev>
2025-12-28 17:05:50 +0100

    module(home): Add desktop entry for bluetui

--- modules/home/programs/bluetui/default.nix
+++ modules/home/programs/bluetui/default.nix
@@ -5,6 +5,7 @@
   ...
 }:
 let
+  inherit (lib) types;
   libcfg = lib.nixsys.home;
 
   bluetoothEnabled = libcfg.isBluetoothEnabled config;
@@ -16,10 +17,27 @@ in
     enable = lib.mkEnableOption "nixsys.home.programs.bluetui" // {
       default = bluetoothEnabled;
     };
+    add-desktop-entry = lib.mkOption {
+      type = types.bool;
+      default = true;
+    };
     package = lib.mkPackageOption pkgs "bluetui" { };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
+
+    xdg.desktopEntries = lib.optionalAttrs cfg.add-desktop-entry {
+      bluetui = {
+        name = "BlueTUI";
+        comment = "Open bluetui in a new $TERMINAL";
+        exec = "${cfg.package}/bin/bluetui";
+        terminal = true;
+        categories = [
+          "System"
+          "ConsoleOnly"
+        ];
+      };
+    };
   };
 }
