0f191e415f01b1bb437ad05062bcf247e633b19e
Wibowo Arindrarto <contact@arindrarto.dev>
2026-01-03 05:40:21 +0100

    overlay: Add temporary fix for broken polybar build in unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,10 +7,27 @@
     unstable = import inputs.nixpkgs-unstable {
       inherit (prev.stdenv.hostPlatform) system;
       config.allowUnfree = true;
+
+      overlays = [
+        (final: prev: {
+          polybar =
+            let
+              patch = final.fetchpatch {
+                name = "gcc15-cstdint-fix.patch";
+                url = "https://github.com/polybar/polybar/commit/f99e0b1c7a5b094f5a04b14101899d0cb4ece69d.patch";
+                sha256 = "sha256-Mf9R4u1Kq4yqLqTFD5ZoLjrK+GmlvtSsEyRFRCiQ72U=";
+              };
+            in
+            prev.polybar.overrideAttrs (old: {
+              patches = old.patches ++ [ patch ];
+            });
+        })
+      ];
     };
     local = import ../packages { pkgs = final; };
   };
 
+  # Permanent modifications.
   modifications = _final: prev: {
     # To speed up builds.
     rustup = prev.rustup.overrideAttrs (_: {
