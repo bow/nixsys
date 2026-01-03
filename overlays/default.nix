66ee67ac0fc7589d5d098dabfb44d7a798199f1d
Wibowo Arindrarto <contact@arindrarto.dev>
2026-01-03 06:15:19 +0100

    overlay: Refactor common overlays and define unstable-only overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,33 +2,9 @@
   inputs,
   ...
 }:
-{
-  additions = final: prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      inherit (prev.stdenv.hostPlatform) system;
-      config.allowUnfree = true;
-
-      overlays = [
-        (final: prev: {
-          polybar =
-            let
-              patch = final.fetchpatch {
-                name = "gcc15-cstdint-fix.patch";
-                url = "https://github.com/polybar/polybar/commit/f99e0b1c7a5b094f5a04b14101899d0cb4ece69d.patch";
-                sha256 = "sha256-Mf9R4u1Kq4yqLqTFD5ZoLjrK+GmlvtSsEyRFRCiQ72U=";
-              };
-            in
-            prev.polybar.overrideAttrs (old: {
-              patches = old.patches ++ [ patch ];
-            });
-        })
-      ];
-    };
-    local = import ../packages { pkgs = final; };
-  };
-
+let
   # Permanent modifications.
-  modifications = _final: prev: {
+  modificationsCommon = _final: prev: {
     # To speed up builds.
     rustup = prev.rustup.overrideAttrs (_: {
       doCheck = false;
@@ -36,13 +12,36 @@
     terraform = prev.terraform.overrideAttrs (_: {
       doCheck = false;
     });
-    unstable = prev.unstable // {
-      rustup = prev.unstable.rustup.overrideAttrs (_: {
-        doCheck = false;
+  };
+
+  # Temporary fixes.
+  fixesUnstable = final: prev: {
+    polybar =
+      let
+        patch = final.fetchpatch {
+          name = "gcc15-cstdint-fix.patch";
+          url = "https://github.com/polybar/polybar/commit/f99e0b1c7a5b094f5a04b14101899d0cb4ece69d.patch";
+          sha256 = "sha256-Mf9R4u1Kq4yqLqTFD5ZoLjrK+GmlvtSsEyRFRCiQ72U=";
+        };
+      in
+      prev.polybar.overrideAttrs (old: {
+        patches = old.patches ++ [ patch ];
       });
-      terraform = prev.unstable.terraform.overrideAttrs (_: {
-        doCheck = false;
-      });
+  };
+in
+{
+  additions = final: prev: {
+    local = import ../packages { pkgs = final; };
+    unstable = import inputs.nixpkgs-unstable {
+      inherit (prev.stdenv.hostPlatform) system;
+      config.allowUnfree = true;
+
+      overlays = [
+        modificationsCommon
+        fixesUnstable
+      ];
     };
   };
+
+  modifications = modificationsCommon;
 }
